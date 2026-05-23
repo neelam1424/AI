264a67f13a4ee9391df3126d82898533f1508c2d
Neelam More <neelammore1404@gmail.com>
2026-05-23 17:43:09 -0400

    dictionary

---
+++ Python/dictionary.py
@@ -0,0 +1,7 @@
+model_result = {
+    "model": "Random Forest",
+    "accuracy": 0.91,
+    "f1_score": 0.89,
+}
+
+print(f"Model Result of {model_result['model']} got accuracy {model_result['accuracy']}")
