69992c706d9633959605e906b6e355e908297d7e
Neelam More <neelammore1404@gmail.com>
2026-05-23 17:40:04 -0400

    list

---
+++ Python/lists.py
@@ -0,0 +1,8 @@
+student_marks = [85,90,78,92]
+print(student_marks)
+student_marks.append(69)
+print(student_marks)
+
+average = sum(student_marks) / len(student_marks)
+
+print("Average Marks:", average)
