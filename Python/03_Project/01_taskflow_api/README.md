eab7939b8ca8e8a0d441eca649fcca503d6f1ca6
Neelam More <neelammore1404@gmail.com>
2026-06-08 15:30:09 -0400

    Setup PostgreSQL + SQLAlchemy Database connection

---
+++ Python/03_Project/01_taskflow_api/README.md
@@ -0,0 +1,17 @@
+
+Step 0: Understand the project problem
+Step 1: Create project setup
+Step 2: Setup PostgreSQL connection
+Step 3: Setup SQLAlchemy Base + Session
+Step 4: Setup Alembic
+Step 5: Build User Signup
+Step 6: Build Login
+Step 7: Add JWT Auth
+Step 8: Add Protected Route
+Step 9: Build Workspace
+Step 10: Build Project
+Step 11: Build Task
+Step 12: Assign Task
+Step 13: Change Task Status
+Step 14: Add RBAC
+Step 15: Add tests
--- Python/03_Project/01_taskflow_api/app/main.py
+++ Python/03_Project/01_taskflow_api/app/main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from sqlalchemy import text
+
+from app.db.session import SessionLocal
 
 app = FastAPI(
     title = "TaskFlow API",
@@ -11,4 +14,15 @@ def root():
     return {
         "message": "TaskFlow API is running"
     }
+
+
+@app.get("/db-test")
+def db_test():
+    db = SessionLocal()
+
+    try:
+        db.execute(text("SELECT 1"))
+        return {"message": "Database connected successfully"}
+    finally:
+        db.close()
 
